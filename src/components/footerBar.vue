3082eb4a1fe6ddb7473a6999a94f737f4bb96b98
cjw931027 <cjw931027@gmail.com>
2025-08-15 16:07:16 +0800

    feat:新增頁尾

---
+++ src/components/footerBar.vue
@@ -0,0 +1,26 @@
+<script></script>
+
+<template>
+  <footer class="bg-gray-50 py-4">
+    <div class="text-center text-gray-500 text-sm space-y-1">
+      <p>© 2025 臺北市立大學 資訊科學系(含碩士班) 版權所有</p>
+      <p>
+        Copyright © 2025 University of Taipei Department of Computer Science. All Rights Reserved.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  background-color: #f9f9f9;
+  padding: 1rem 0;
+}
+
+footer p {
+  margin: 0;
+  color: #6b7280; /* 灰色字 */
+  font-size: 0.875rem; /* text-sm */
+  text-align: center;
+}
+</style>
